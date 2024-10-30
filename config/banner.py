771bf32e381018a3b05fe82ae33b0c17f83daac1
parikesit030 <64907207+parikesit030@users.noreply.github.com>
2024-10-30 01:16:00 +0000

    banner.py

---
+++ config/banner.py
@@ -0,0 +1,5 @@
+def show_banner():
+    print(f"============ Parikesit030 ============")
+    print(f"I edited it a little so that it works on Android Termux")
+    print(f"original script created by: https://github.com/Galkurta")
+    print(f"====================================")
